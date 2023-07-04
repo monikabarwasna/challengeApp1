14e08cbee26a01be549cad4792117df391865903
monika51016 <monika.barwasna@gmail.com>
2023-07-04 14:38:21 +0200

    zadanie domowe dzień 4

---
+++ challengeApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿var name = "Ewa";
+var age = 33;
+bool female = true;
+
+if (female)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("kobieta poniżej 30 lat");
+
+    }
+    else if (age > 30)
+    {
+        Console.WriteLine($"{name} lat {age}");
+
+    }
+}
+else if (!female)
+{
+    if (age < 18)
+    {
+        Console.WriteLine("niepełnoletni mężczyzna");
+    }
+    else if (age > 18)
+    {
+        Console.WriteLine("pełnoletni mężczyzna");
+    }
+}
